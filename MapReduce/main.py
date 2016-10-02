93e6ef51513460e9811f0aa182c96b7120b008bc
Aishwarya Kumar <aishwaryaksharma@gmail.com>
2016-10-02 17:05:17 +0530

    Removed wrong description

--- MapReduce/main.py
+++ MapReduce/main.py
@@ -1,11 +1,3 @@
-######################################################################################################
-########################################### ALGORITHM ################################################
-######################################################################################################
-# 1. On computing the the first few rows of the triangle , we notice a pattern that the first even
-#    number is at the position 2, 3, 2, 4, 2, 3, 2, 4, 2, 3, 2, 4..... (starting from row 3). 
-#    So basically the position of the first even number repeats after an interval of 4 in the above fashion.
-# 2. Use this property to quickly and efficiently find the answer.
-######################################################################################################
 import sys
 from collections import OrderedDict
 class MapReduce:
